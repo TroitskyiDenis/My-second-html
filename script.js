c3b79149f6a500b8bfb65def10de7bb212727290
MacBook <macbook@MacBook-Pro-MacBook.local>
2023-10-10 16:37:14 +0300

    Second html

---
+++ script.js
@@ -0,0 +1,49 @@
+// Задание 1
+// function hello(){
+//     document.getElementById("demo").innerHTML = "Hello, World";
+// }
+// Задание 2
+// alert="Привет мир!";
+// Задание 3
+
+// let num1 = parseInt(prompt("Введите 1-ое число: "));
+// let num2 = parseInt(prompt("Введите 2-ое число: "));
+// alert(`${num1}+${num2} = ${num1+num2}`);
+// Задание 4
+// var names = prompt("Введите имя: ","Name");
+// alert(`Привет,${names}!`);
+// Задание 5
+// function hello_world(name){
+//     alert(`Привет,${name}! `);
+// }
+// let names = prompt("Введите имя: ","Name");
+// hello_world(names);
+ // Задание 6
+//  if (confirm("Вы уверены? "))
+//  alert("Мы рады, что Вы уверены! ");
+// else
+// alert("Жаль, что Вы не уверены");
+// let n = (confirm("Вы уверены?")) ?  alert("Мы рады, что Вы уверены! ") : alert("Жаль, что Вы не уверены");
+// Задание 7
+
+// let age = parseInt(prompt("Введите свой возраст: ", "возраст"));
+// switch(age){
+//     case 18:
+//         alert("Вы совершеннолетний, Вам все можно!");
+//          break;
+//     case 10:
+//         alert("Вам надо учить уроки!");
+//         break;
+//     case 30:
+//         alert("Ложитесь спать, Вам завтра на работу!");
+//         break;
+//     default:
+//         alert("Мы не знаем, что с Вам делать!");
+//         break;             
+// } 
+//  Домашнее задание:  Создать страницу, которая спрашивает имя у пользователя
+//   и выводит его с помощью функции.
+
+
+ let names = prompt("Введите ваше  имя: ","name");
+ alert(`Привет,${names}!`);
